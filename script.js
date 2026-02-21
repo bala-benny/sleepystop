a5911a65717cf50df5b9eee521b7c4c9492edf4d
Gayathri <gayathrisanthoshpc3008@gmail.com>
2026-02-21 09:58:54 +0530

    Added geocoding to convert place name into coordinates

--- script.js
+++ script.js
@@ -82,3 +82,38 @@ function startRealTracking() {
     }
   );
 }
+function calculateDistance(lat1, lon1, lat2, lon2) {
+
+  const R = 6371; // Earth radius in km
+
+  const dLat = (lat2 - lat1) * Math.PI / 180;
+  const dLon = (lon2 - lon1) * Math.PI / 180;
+
+  const a =
+    Math.sin(dLat/2) * Math.sin(dLat/2) +
+    Math.cos(lat1 * Math.PI / 180) *
+    Math.cos(lat2 * Math.PI / 180) *
+    Math.sin(dLon/2) * Math.sin(dLon/2);
+
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+  return R * c;
+}
+async function getCoordinates(place) {
+
+  let response = await fetch(
+    `https://nominatim.openstreetmap.org/search?format=json&q=${place}`
+  );
+
+  let data = await response.json();
+
+  if (data.length === 0) {
+    alert("Place not found!");
+    return null;
+  }
+
+  return {
+    lat: parseFloat(data[0].lat),
+    lon: parseFloat(data[0].lon)
+  };
+}
